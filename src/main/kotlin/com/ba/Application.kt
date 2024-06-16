488b6292e60c0218a8fcba60589b22de57143ba5
Ronjar <robingebert99@gmail.com>
2024-06-16 15:47:25 +0200

    Changed back to 0.0.0.0

--- src/main/kotlin/com/ba/Application.kt
+++ src/main/kotlin/com/ba/Application.kt
@@ -9,7 +9,7 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = 80, host = "127.0.0.1", module = Application::module)
+    embeddedServer(Netty, port = 80, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
