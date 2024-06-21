66eadca64e0c48253f32784feb66520e0b158e40
Ronjar <robingebert99@gmail.com>
2024-06-21 13:08:47 +0200

    New frontend version

--- src/main/resources/frontend/_app/immutable/entry/start.CATJWL3r.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.BUMzI43k.js";export{t as start};
---
+++ src/main/resources/frontend/_app/immutable/nodes/3.DqAZOFby.js
@@ -0,0 +1,1 @@
+import{s as n,o as s}from"../chunks/scheduler.Du_WS53y.js";import{S as e,i as a}from"../chunks/index.BEvaicvu.js";import{g as r}from"../chunks/entry.Bf4NNE8X.js";function i(o){return s(async()=>{r("/privacy")}),[]}class u extends e{constructor(t){super(),a(this,t,i,null,n,{})}}export{u as component};
